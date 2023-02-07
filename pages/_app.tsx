8a7fa532569f79a10382a602318cf44312931b96
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2023-02-07 13:25:13 -0500

    new link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export const GlobalProfileContextDefault = {
   openFrame: false,
   setOpenFrame: (expressInterest: boolean) => {},
   newLink:
-    "https://www.youtube.com/embed/VIsW_zH2KHw?controls=0?rel=0&autoplay=1&mute=1",
+    "https://www.youtube.com/embed/KXf6puIS-bo?controls=0?rel=0&autoplay=1&mute=1",
 };
 
 export const GlobalContext = createContext(GlobalProfileContextDefault);
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [newLink, setNewLink] = useState(GlobalProfileContextDefault.newLink);
   const streamLinks: string[] = [
     "https://www.youtube.com/embed/KXf6puIS-bo?controls=0?rel=0&autoplay=1&mute=1",
-    "https://www.youtube.com/embed/VIsW_zH2KHw?controls=0?rel=0&autoplay=1&mute=1",
+    "https://www.youtube.com/embed/KXf6puIS-bo?controls=0?rel=0&autoplay=1&mute=1",
   ];
   useEffect(() => {
     const shuffledLinks: number[] = shuffle([0, 1]);
